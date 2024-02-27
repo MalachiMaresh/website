378813d71a23cb3c8de178cb62881448e9710ca3
Malachi Maresh <88845933+MalachiMaresh@users.noreply.github.com>
2024-02-27 17:56:42 +0000

    Tue, Feb 27, 2024, 9:56 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,15 @@
 loadText = document.querySelector('.loading-text')
 
+bg = document.querySelector('.bg')
+
 load = 90
 
-loadText.innerText = `${load}`%
+
+loadText.style.opacity = 1
+bg.style.filter = "blur(10px)"
 
 
+function blurring() {
+    load = load + 1
+    loadText.innerText = `${load}`%
+}
