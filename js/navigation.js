e47bb7a073a5d719dffc3a1f29dcf23c11e383ac
Malachi Maresh <88845933+MalachiMaresh@users.noreply.github.com>
2024-03-06 17:11:41 +0000

    Wed, Mar 6, 2024, 9:11 AM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -3,9 +3,9 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if (window.scrollY > 223) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active)
-    }
+  if (window.scrollY > 223) {
+    nav.classList.add('active')
+  } else {
+    nav.classList.remove('active')
+  }
 }
