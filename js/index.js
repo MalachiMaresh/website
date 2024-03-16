ff5f92f1a50bfc8c89e314072767a94aa4d30aca
Malachi Maresh <88845933+MalachiMaresh@users.noreply.github.com>
2024-03-16 04:16:08 +0000

    Fri, Mar 15, 2024, 9:16 PM -07:00

---
+++ js/index.js
@@ -0,0 +1,19 @@
+one = document.querySelector('.one')
+two = document.querySelector('.two')
+three = document.querySelector('.three')
+four = document.querySelector('.four')
+five = document.querySelector('.five')
+six = document.querySelector('.six')
+
+change = document.querySelector('.change')
+
+
+change.addEventListener('click', () => {
+    one.style.zIndex = Math.floor(Math.random() * 10)
+    two.style.zIndex = Math.floor(Math.random() * 10)
+    three.style.zIndex = Math.floor(Math.random() * 10)
+    four.style.zIndex = Math.floor(Math.random() * 10)
+    five.style.zIndex = Math.floor(Math.random() * 10)
+    six.style.zIndex = Math.floor(Math.random() * 10)
+
+})
